6de1dc4778bcd00631b8af9c41f2126aa0362aaf
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-12-03 18:17:18 +0100

    Add game phase duration validation

--- sql/validation.sql
+++ sql/validation.sql
@@ -1,7 +1,15 @@
+-- game phase durations should not be negative
 select * from game_phases gp
 join matches m on m.id = gp.match_id_fk
 where gp.duration < '0s'::interval;
 
+-- there should be no very long game phases
 select * from game_phases gp
                   join matches m on m.id = gp.match_id_fk
 where gp.duration > '1h'::interval;
+
+-- sum of game phase duration should the same as match duration
+select matches.file_name, duration, duration_sum, duration - duration_sum as diff from matches
+join (select match_id_fk, sum(game_phases.duration) duration_sum from game_phases
+group by match_id_fk) as mifds on match_id_fk = matches.id
+order by diff desc;
