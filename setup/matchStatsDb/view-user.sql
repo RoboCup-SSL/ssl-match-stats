c9612587b143a3b9e5c7651abd3f5bb053aa771a
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-12-03 12:29:16 +0100

    Update command for view-user permissions

--- setup/matchStatsDb/view-user.sql
+++ setup/matchStatsDb/view-user.sql
@@ -1,8 +1,2 @@
-grant select on table tournaments to ssl_match_stats_view;
-grant select on table matches to ssl_match_stats_view;
-grant select on table team_match_stats to ssl_match_stats_view;
-grant select on table game_phases to ssl_match_stats_view;
-grant select on table auto_refs to ssl_match_stats_view;
-grant select on table game_events to ssl_match_stats_view;
-grant select on table game_event_auto_ref_mapping to ssl_match_stats_view;
-grant select on table game_phase_duration_per_type to ssl_match_stats_view;
+-- allow all users, including view user, to select from all tables and views
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO PUBLIC;
