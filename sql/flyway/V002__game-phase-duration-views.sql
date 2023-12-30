57eec17c833f0e198fbdea1dc2a2837fe50abdf0
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-12-30 12:32:28 +0100

    Rework game phase / match duration views

--- sql/flyway/V002__game-phase-duration-views.sql
+++ sql/flyway/V002__game-phase-duration-views.sql
@@ -1,63 +1,53 @@
--- the view can be used with Metabase to great a histogram of game phase durations
-create view game_phase_duration as
-select id, type, extract(epoch FROM duration) duration_seconds
-from game_phases;
-
-create view game_phase_duration_per_match AS
+create view game_phase_durations as
 select gp.match_id_fk,
        gp.type,
-       count(*)                                                  as count,
-       sum(gp.duration)                                          as total,
-       min(gp.duration)                                          as min,
-       avg(gp.duration)                                          as avg,
-       max(gp.duration)                                          as max,
-       percentile_disc(0.25) within group (order by gp.duration) as p25,
-       percentile_disc(0.5) within group (order by gp.duration)  as median,
-       percentile_disc(0.75) within group (order by gp.duration) as p75
-from game_phases gp
-group by gp.type, gp.match_id_fk;
+       gp.for_team,
+       extract(epoch FROM gp.duration) as duration
+from game_phases gp;
 
-create view game_phase_duration_per_team AS
-select m.team_name,
-       gp.type,
-       count(*)                                                  as count,
-       sum(gp.duration)                                          as total,
-       min(gp.duration)                                          as min,
-       avg(gp.duration)                                          as avg,
-       max(gp.duration)                                          as max,
-       percentile_disc(0.25) within group (order by gp.duration) as p25,
-       percentile_disc(0.5) within group (order by gp.duration)  as median,
-       percentile_disc(0.75) within group (order by gp.duration) as p75
-from game_phases gp
-         join team_match_stats m on m.match_id_fk = gp.match_id_fk
-group by m.team_name, gp.type;
-
-create view game_phase_duration_per_tournament AS
+create view match_durations_by_type AS
 select m.tournament_name,
-       m.division,
-       gp.type,
-       count(*)                                                  as count,
-       sum(gp.duration)                                          as total,
-       min(gp.duration)                                          as min,
-       avg(gp.duration)                                          as avg,
-       max(gp.duration)                                          as max,
-       percentile_disc(0.25) within group (order by gp.duration) as p25,
-       percentile_disc(0.5) within group (order by gp.duration)  as median,
-       percentile_disc(0.75) within group (order by gp.duration) as p75
-from game_phases gp
-         join matches m on m.id = gp.match_id_fk
-group by m.tournament_name, m.division, gp.type;
+       m.division::text,
+       m.file_name,
+       gpd.type::text,
+       count(*)                                                   as count,
+       sum(gpd.duration)                                          as total,
+       sum(gpd.duration) / 60                                     as total_minutes,
+       min(gpd.duration)                                          as min,
+       avg(gpd.duration)                                          as avg,
+       max(gpd.duration)                                          as max,
+       percentile_disc(0.25) within group (order by gpd.duration) as p25,
+       percentile_disc(0.5) within group (order by gpd.duration)  as median,
+       percentile_disc(0.75) within group (order by gpd.duration) as p75
+from game_phase_durations gpd
+         join matches m on m.id = gpd.match_id_fk
+group by m.tournament_name, m.division, m.file_name, gpd.type;
 
-create view game_phase_duration_overall AS
-select gp.type,
-       count(*)                                                  as count,
-       sum(gp.duration)                                          as total,
-       min(gp.duration)                                          as min,
-       avg(gp.duration)                                          as avg,
-       max(gp.duration)                                          as max,
-       percentile_disc(0.25) within group (order by gp.duration) as p25,
-       percentile_disc(0.5) within group (order by gp.duration)  as median,
-       percentile_disc(0.75) within group (order by gp.duration) as p75
-from game_phases gp
-         join team_match_stats m on m.match_id_fk = gp.match_id_fk
-group by gp.type;
+create view match_durations_by_type_per_team AS
+select m.tournament_name,
+       m.division::text,
+       tms.team_name,
+       gpd.type::text,
+       count(*)                                                   as count,
+       sum(gpd.duration)                                          as total,
+       sum(gpd.duration) / 60                                     as total_minutes,
+       min(gpd.duration)                                          as min,
+       avg(gpd.duration)                                          as avg,
+       max(gpd.duration)                                          as max,
+       percentile_disc(0.25) within group (order by gpd.duration) as p25,
+       percentile_disc(0.5) within group (order by gpd.duration)  as median,
+       percentile_disc(0.75) within group (order by gpd.duration) as p75
+from game_phase_durations gpd
+         join matches m on m.id = gpd.match_id_fk
+         join team_match_stats tms on tms.match_id_fk = gpd.match_id_fk
+where gpd.for_team = 'NONE'
+   or gpd.for_team = tms.team_color
+group by m.tournament_name, m.division, tms.team_name, gpd.type;
+
+create view match_durations as
+select m.tournament_name,
+       m.division::text,
+       m.file_name,
+       extract(epoch FROM m.duration)      as duration,
+       extract(epoch FROM m.duration) / 60 as duration_minutes
+from matches m;
