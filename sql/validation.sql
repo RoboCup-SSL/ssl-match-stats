b1863664ca7d5e3399a7c116580228ba57a35dbe
Nicolai Ommer <nicolai.ommer@gmail.com>
2024-01-27 13:23:49 +0100

    Validate consistency between game event type and category metrics

--- sql/validation.sql
+++ sql/validation.sql
@@ -25,8 +25,70 @@ from team_match_stats
 order by team_name;
 
 -- check sum of accepted/proposed/withdrawn events
-select type,
+select game_event_type,
        total,
        accepted + accepted_withdrawn + proposed + proposed_withdrawn as sum
-from game_event_type_metrics_overall
+from game_event_type_metrics
 where total <> accepted + accepted_withdrawn + proposed + proposed_withdrawn;
+select game_event_type,
+       total,
+       accepted + accepted_withdrawn + proposed + proposed_withdrawn as sum
+from game_event_type_metrics_per_team
+where total <> accepted + accepted_withdrawn + proposed + proposed_withdrawn;
+select category,
+       total,
+       accepted + accepted_withdrawn + proposed + proposed_withdrawn as sum
+from game_event_category_metrics
+where total <> accepted + accepted_withdrawn + proposed + proposed_withdrawn;
+select category,
+       total,
+       accepted + accepted_withdrawn + proposed + proposed_withdrawn as sum
+from game_event_category_metrics_per_team
+where total <> accepted + accepted_withdrawn + proposed + proposed_withdrawn;
+
+-- check Game Event Type vs. Category stats
+select c.tournament_name,
+       c.category,
+       t.total,
+       c.total,
+       t.accepted,
+       c.accepted
+from (select game_event_category,
+             tournament_name,
+             sum(total)    total,
+             sum(accepted) accepted
+      from game_event_type_metrics c
+      group by game_event_category, tournament_name) as t
+         join (select category,
+                      tournament_name,
+                      sum(total)    total,
+                      sum(accepted) accepted
+               from game_event_category_metrics
+               group by category, tournament_name) c
+              on c.category = t.game_event_category and c.tournament_name = t.tournament_name
+where t.total <> c.total
+   or t.accepted <> c.accepted;
+select c.tournament_name,
+       c.team_name,
+       c.category,
+       t.total,
+       c.total,
+       t.accepted,
+       c.accepted
+from (select game_event_category,
+             tournament_name,
+             team_name,
+             sum(total)    total,
+             sum(accepted) accepted
+      from game_event_type_metrics_per_team c
+      group by game_event_category, tournament_name, team_name) as t
+         join (select category,
+                      tournament_name,
+                      team_name,
+                      sum(total)    total,
+                      sum(accepted) accepted
+               from game_event_category_metrics_per_team
+               group by category, tournament_name, team_name) c
+              on c.category = t.game_event_category and c.tournament_name = t.tournament_name and c.team_name = t.team_name
+where t.total <> c.total
+   or t.accepted <> c.accepted;
