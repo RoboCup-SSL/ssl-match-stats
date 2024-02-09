b69c597b1d5616aa83a094d4163b83a44a1edf97
Nicolai Ommer <nicolai.ommer@gmail.com>
2024-02-09 20:03:51 +0100

    Use total robot count (both teams) for per match and tournament views

--- sql/flyway/V006__robot-count-views.sql
+++ sql/flyway/V006__robot-count-views.sql
@@ -1,3 +1,11 @@
+create view robot_count_total as
+select rc.game_phase_id_fk,
+       rc.start_time,
+       rc.duration,
+       sum(rc.count) as count
+from robot_count rc
+group by rc.game_phase_id_fk, rc.start_time, rc.duration;
+
 drop view if exists robot_count_per_match cascade;
 create view robot_count_per_match as
 select s.match_id_fk,
@@ -8,14 +16,14 @@ select s.match_id_fk,
 from (select gp.match_id_fk,
              rc.count,
              sum(rc.duration) as duration
-      from robot_count rc
+      from robot_count_total rc
                join game_phases gp on rc.game_phase_id_fk = gp.id
       where gp.type in ('RUNNING', 'PREPARE_KICKOFF', 'PREPARE_PENALTY', 'STOP', 'BALL_PLACEMENT')
         and gp.stage in ('NORMAL_FIRST_HALF', 'NORMAL_SECOND_HALF', 'EXTRA_FIRST_HALF', 'EXTRA_SECOND_HALF')
       group by match_id_fk, count) as s
          join (select gp.match_id_fk,
                       sum(rc.duration) as total_duration
-               from robot_count rc
+               from robot_count_total rc
                         join game_phases gp on rc.game_phase_id_fk = gp.id
                where gp.type in ('RUNNING', 'PREPARE_KICKOFF', 'PREPARE_PENALTY', 'STOP', 'BALL_PLACEMENT')
                  and gp.stage in ('NORMAL_FIRST_HALF', 'NORMAL_SECOND_HALF', 'EXTRA_FIRST_HALF', 'EXTRA_SECOND_HALF')
@@ -33,7 +41,7 @@ from (select m.tournament_name,
              m.division,
              rc.count,
              sum(rc.duration) as duration
-      from robot_count rc
+      from robot_count_total rc
                join game_phases gp on rc.game_phase_id_fk = gp.id
                join matches m on gp.match_id_fk = m.id
       where gp.type in ('RUNNING', 'PREPARE_KICKOFF', 'PREPARE_PENALTY', 'STOP', 'BALL_PLACEMENT')
@@ -42,7 +50,7 @@ from (select m.tournament_name,
          join (select m.tournament_name,
                       m.division,
                       sum(rc.duration) as total_duration
-               from robot_count rc
+               from robot_count_total rc
                         join game_phases gp on rc.game_phase_id_fk = gp.id
                         join matches m on gp.match_id_fk = m.id
                where gp.type in ('RUNNING', 'PREPARE_KICKOFF', 'PREPARE_PENALTY', 'STOP', 'BALL_PLACEMENT')
@@ -79,10 +87,20 @@ from (select tms.team_name,
                         join matches m on gp.match_id_fk = m.id
                         join team_match_stats tms on m.id = tms.match_id_fk and tms.team_color = rc.team_color
                where gp.type in ('RUNNING', 'PREPARE_KICKOFF', 'PREPARE_PENALTY', 'STOP', 'BALL_PLACEMENT')
-               and gp.stage in ('NORMAL_FIRST_HALF', 'NORMAL_SECOND_HALF', 'EXTRA_FIRST_HALF', 'EXTRA_SECOND_HALF')
+                 and gp.stage in ('NORMAL_FIRST_HALF', 'NORMAL_SECOND_HALF', 'EXTRA_FIRST_HALF', 'EXTRA_SECOND_HALF')
                group by tms.team_name, m.tournament_name, division) as t
               on s.tournament_name = t.tournament_name and s.division = t.division and s.team_name = t.team_name
 order by s.team_name, s.tournament_name, s.division, s.count;
+
+drop view if exists robot_count_avg_per_match cascade;
+create view robot_count_avg_per_match as
+select rc.match_id_fk,
+       rc.tournament_name,
+       rc.division,
+       sum(rc.count * rc.duration_percentage) / 100 as avg_count
+from robot_count_per_match rc
+group by rc.match_id_fk, tournament_name, division
+order by tournament_name, division;
 
 drop view if exists robot_count_avg_per_tournament cascade;
 create view robot_count_avg_per_tournament as
